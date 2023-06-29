5e7a8ad55357bf23a1202b6ba6c0846796716b50
vlad.povarna <vlad.povarna@uipath.com>
2023-06-29 14:07:21 +0300

    Initial commit

--- src/main/java/io/aoc/_2015/day01/Problem.java
+++ src/main/java/io/aoc/_2015/day01/Problem.java
@@ -4,13 +4,40 @@ import io.aoc._2015.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class Problem {
-    private static Logger logger = LoggerFactory.getLogger(Problem.class);
+    private static final Logger logger = LoggerFactory.getLogger(Problem.class);
     public static void main(String[] args) {
-        Utils utils = new Utils();
-        List<String> strings = utils.readInputFile("1", "input.txt");
-        strings.forEach(line -> logger.info(line));
+        var problem = new Problem();
+        var is = problem.getFileFromResourceAsStream("day1/input.txt");
+        logger.info("test");
+//        Files.readString(Path.of())
+
+//        Utils utils = new Utils();
+//        List<String> strings = utils.readInputFile("1", "input.txt");
+//        strings.forEach(logger::info);
+    }
+
+    private InputStream getFileFromResourceAsStream(String fileName) {
+
+        // The class loader that loaded the class
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+
+        // the stream holding the file content
+        if (inputStream == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return inputStream;
+        }
+
     }
 }
