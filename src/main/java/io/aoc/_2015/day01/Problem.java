356444184c3384511c5f19d812dcfe420ee66b50
vlad.povarna <vlad.povarna@uipath.com>
2023-07-03 09:40:40 +0300

    rebase

--- src/main/java/io/aoc/_2015/day01/Problem.java
+++ src/main/java/io/aoc/_2015/day01/Problem.java
@@ -4,44 +4,13 @@ import io.aoc._2015.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.stream.IntStream;
+import java.util.List;
 
 public class Problem {
-    private static final Logger logger = LoggerFactory.getLogger(Problem.class);
-
-    public int part1(String input) {
-        return getCaloriesSums(input).max().orElse(0);
-    }
-
-    public int part2(String input) {
-        return getCaloriesSums(input)
-                .boxed()
-                .sorted(Comparator.reverseOrder())
-                .limit(3)
-                .mapToInt(Integer::valueOf)
-                .sum();
-    }
-
-    private IntStream getCaloriesSums(String input) {
-        final String[] blocks = input.split("\\n\\n");
-        return Arrays.stream(blocks)
-                .mapToInt(this::sum);
-    }
-
-    private int sum(String input) {
-        return input.lines()
-                .mapToInt(Integer::parseInt)
-                .sum();
-    }
-
+    private static Logger logger = LoggerFactory.getLogger(Problem.class);
     public static void main(String[] args) {
-        final String inputFile = Utils.readInputFileAsString(1, "input.txt");
-        final Problem problem = new Problem();
-
-        logger.info("Aoc2022, Day1 Problem, Part1: " + problem.part1(inputFile));
-        logger.info("Aoc2022, Day1 Problem, Part1: " + problem.part2(inputFile));
+        Utils utils = new Utils();
+        List<String> strings = utils.readInputFile("1", "input.txt");
+        strings.forEach(line -> logger.info(line));
     }
-
 }
