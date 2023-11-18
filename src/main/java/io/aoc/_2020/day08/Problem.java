35a6d767334b26c1629de3c340967efb4750dbd9
vlad.povarna <vlad.povarna@uipath.com>
2023-11-18 11:28:14 +0200

    Initial scaffolding

---
+++ src/main/java/io/aoc/_2020/day08/Problem.java
@@ -0,0 +1,26 @@
+package io.aoc._2020.day08;
+
+import io.aoc.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Problem {
+    private static final Logger logger = LoggerFactory.getLogger(Problem.class);
+
+    public static void main(String[] args) {
+        var problem = new Problem();
+        var input = Utils.readInputFileAsString(2020, 7, "input.txt");
+        logger.info("Aoc2020, Day8 Problem, Part1: {}", problem.part1(input));
+        logger.info("Aoc2020, Day8 Problem, Part2: {}", problem.part2(input));
+    }
+
+    public int part1(String input) {
+        return 0;
+    }
+
+    public int part2(String input) {
+        return 0;
+    }
+
+
+}
---
+++ src/test/java/io/aoc/_2020/day08/ProblemTest.java
@@ -0,0 +1,16 @@
+package io.aoc._2020.day08;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProblemTest {
+
+    @Test
+    void part1Test() {
+    }
+
+    @Test
+    void part2Test() {
+    }
+}
