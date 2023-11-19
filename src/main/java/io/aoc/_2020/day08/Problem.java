91775b1795fed93cee5df14e0fa8d50b3680d925
povarna <vlad.povarna@uipath.com>
2023-11-19 22:40:30 +0200

    AoC2020 Day08, Part1 & Part2 solutions

--- src/main/java/io/aoc/_2020/day08/Problem.java
+++ src/main/java/io/aoc/_2020/day08/Problem.java
@@ -1,26 +1,117 @@
 package io.aoc._2020.day08;
 
+import io.aoc.utils.Constants;
 import io.aoc.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.*;
 
 public class Problem {
     private static final Logger logger = LoggerFactory.getLogger(Problem.class);
 
     public static void main(String[] args) {
         var problem = new Problem();
-        var input = Utils.readInputFileAsString(2020, 7, "input.txt");
+        var input = Utils.readInputFileAsString(2020, 8, "input.txt");
         logger.info("Aoc2020, Day8 Problem, Part1: {}", problem.part1(input));
         logger.info("Aoc2020, Day8 Problem, Part2: {}", problem.part2(input));
     }
 
     public int part1(String input) {
-        return 0;
+        List<Instruction> instructions = parseInput(input);
+        return processInstructions(instructions).acc();
     }
 
     public int part2(String input) {
-        return 0;
+        var patchedInstructions = patch(parseInput(input));
+        return patchedInstructions.stream()
+                .map(this::processInstructions)
+                .filter(Result::terminated)
+                .findFirst()
+                .orElse(new Result(0, true))
+                .acc();
+    }
+
+    private List<List<Instruction>> patch(List<Instruction> instructions) {
+        var patchedInstructions = new ArrayList<List<Instruction>>();
+        patchedInstructions.add(instructions);
+
+        var i = 0;
+
+        while (i < instructions.size()) {
+            var tmpInstructions = new ArrayList<>(instructions);
+            var currentInstruction = tmpInstructions.get(i);
+            if (currentInstruction.operation().equals("acc")) {
+                i++;
+                continue;
+            }
+
+            if (currentInstruction.operation().equals("nop")) {
+                tmpInstructions.set(i, new Instruction("jmp", currentInstruction.steps()));
+            } else {
+                tmpInstructions.set(i, new Instruction("nop", currentInstruction.steps()));
+            }
+            patchedInstructions.add(tmpInstructions);
+            i++;
+        }
+
+        return patchedInstructions;
+    }
+    private Result processInstructions(List<Instruction> instructions) {
+        var acc = 0;
+        var i = 0;
+        var seen = new HashSet<Integer>();
+
+        while (true) {
+            if (i >= instructions.size()) {
+                return new Result(acc, true);
+            }
+            if (seen.contains(i)) {
+                return new Result(acc, false);
+            } else {
+                seen.add(i);
+                var instruction = instructions.get(i);
+                switch (instruction.operation()) {
+                    case "nop" -> i++;
+                    case "acc" -> {
+                        i++;
+                        acc += instruction.steps();
+                    }
+                    case "jmp" -> i += instruction.steps();
+                    default -> throw new IllegalArgumentException("Unknown case");
+                }
+            }
+
+        }
+
+    }
+    private List<Instruction> parseInput(String input) {
+        var lines = input.split(Constants.EOL);
+        return Arrays.stream(lines)
+                .map(this::createInstruction)
+                .toList();
+    }
+
+    private Instruction createInstruction(String line) {
+        var tokens = line.split(Constants.EMPTY_STRING);
+        if (tokens.length != 2) {
+            throw new IllegalArgumentException("Unable to extract two tokens from the input line: " + line);
+        }
+
+        int steps;
+        try {
+            steps = Integer.parseInt(tokens[1]);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Unable transform steps to integer for the input line: " + line);
+        }
+
+        return new Instruction(tokens[0], steps);
     }
 
 
 }
+
+record Instruction(String operation, int steps) {
+}
+
+record Result(Integer acc, boolean terminated){}
--- src/test/java/io/aoc/_2020/day08/ProblemTest.java
+++ src/test/java/io/aoc/_2020/day08/ProblemTest.java
@@ -1,5 +1,6 @@
 package io.aoc._2020.day08;
 
+import io.aoc.utils.Utils;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,9 +9,17 @@ class ProblemTest {
 
     @Test
     void part1Test() {
+        var input = Utils.readInputFileAsString(2020, 8, "input.txt");
+        var problem = new Problem();
+        int result = problem.part1(input);
+        assertEquals(5, result);
     }
 
     @Test
     void part2Test() {
+        var input = Utils.readInputFileAsString(2020, 8, "input.txt");
+        var problem = new Problem();
+        int result = problem.part2(input);
+        assertEquals(8, result);
     }
 }
