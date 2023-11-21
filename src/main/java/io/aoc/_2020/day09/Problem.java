cee27e3b4aa458440f97818c91aa77fc5182fc54
vlad.povarna <vlad.povarna@uipath.com>
2023-11-21 14:53:25 +0200

    AoC 2020 Day9, Part1 and Part2 solutions

--- src/main/java/io/aoc/_2020/day09/Problem.java
+++ src/main/java/io/aoc/_2020/day09/Problem.java
@@ -5,8 +5,7 @@ import io.aoc.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.*;
 
 public class Problem {
     private static final Logger logger = LoggerFactory.getLogger(Problem.class);
@@ -15,7 +14,7 @@ public class Problem {
         var problem = new Problem();
         var input = Utils.readInputFileAsString(2020, 9, "input.txt");
         logger.info("Aoc2020, Day9 Problem, Part1: {}", problem.part1(input, 27));
-        logger.info("Aoc2020, Day9 Problem, Part2: {}", problem.part2(input));
+        logger.info("Aoc2020, Day9 Problem, Part2: {}", problem.part2(input, 27));
     }
 
     public long part1(String input, int preamble) {
@@ -32,8 +31,52 @@ public class Problem {
         return -1;
     }
 
-    public int part2(String input) {
-        return 1;
+    public long part2(String input, int preamble) {
+        var target = part1(input, preamble);
+        var values = parseInput(input);
+
+        var consecutiveNumbersSoFar = getConsecutiveNumbers(target, values);
+        if (consecutiveNumbersSoFar.size() < 2) {
+            throw new RuntimeException("Unable to find the consecutive numbers.");
+        }
+        var minValue = Long.MAX_VALUE;
+        var maxValue = Long.MIN_VALUE;
+
+        for (var n : consecutiveNumbersSoFar) {
+            minValue = Math.min(minValue, n);
+            maxValue = Math.max(maxValue, n);
+        }
+
+        return maxValue + minValue;
+    }
+
+    private List<Long> getConsecutiveNumbers(long target, long[] values) {
+        for (var i = 0; i < values.length; i++) {
+            var consecutiveNumbersSoFar = new ArrayList<Long>();
+            if (values[i] >= target) {
+                continue;
+            }
+
+            consecutiveNumbersSoFar.add(values[i]);
+            for (var j = i + 1; j < values.length - 1; j++) {
+
+                var sum = consecutiveNumbersSoFar.stream().mapToLong(x -> x).sum();
+                long partialSum = sum + values[j];
+                if (partialSum > target) {
+                    // exit
+                    break;
+                }
+
+                if (partialSum < target) {
+                    consecutiveNumbersSoFar.add(values[j]);
+                } else {
+                    consecutiveNumbersSoFar.add(values[j]);
+                    return consecutiveNumbersSoFar;
+                }
+
+            }
+        }
+        return new ArrayList<>();
     }
 
     private long[] parseInput(String input) {
--- src/test/java/io/aoc/_2020/day09/ProblemTest.java
+++ src/test/java/io/aoc/_2020/day09/ProblemTest.java
@@ -16,4 +16,12 @@ class ProblemTest {
         var result = problem.part1(input, 5);
         Assertions.assertEquals(127, result);
     }
+
+    @Test
+    void part2Test() {
+        var problem = new Problem();
+        var input = Utils.readInputFileAsString(2020, 9, "input.txt");
+        var result = problem.part2(input, 5);
+        Assertions.assertEquals(62, result);
+    }
 }
