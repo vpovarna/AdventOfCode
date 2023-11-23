7b0fd31f6c7cb651a19148945f93e2c430bf757c
vlad.povarna <vlad.povarna@uipath.com>
2023-11-23 10:00:41 +0200

    @trivial Covert tuple to record

--- src/main/java/io/aoc/_2020/day10/Problem.java
+++ src/main/java/io/aoc/_2020/day10/Problem.java
@@ -66,28 +66,13 @@ public class Problem {
 
     private long count(ArrayList<Tuple<Integer, Integer>> windows, int target) {
         return windows.stream()
-                .filter(t -> t.getItem1() - t.getItem2() == target)
+                .filter(t -> t.item1() - t.item2() == target)
                 .count();
     }
 
 }
 
-class Tuple<T1, T2> {
-    private final T1 item1;
-    private final T2 item2;
-
-    public Tuple(T1 item1, T2 item2) {
-        this.item1 = item1;
-        this.item2 = item2;
-    }
-
-    public T1 getItem1() {
-        return item1;
-    }
-
-    public T2 getItem2() {
-        return item2;
-    }
+record Tuple<T1, T2>(T1 item1, T2 item2) {
 
     @Override
     public String toString() {
