d776d1e8c987329b0f6b1df24bb835d84da7756a
povarna <vlad.povarna@uipath.com>
2023-11-28 10:48:29 +0200

    AoC2020 Day12 Part1 & Part2 solutions

--- src/main/java/io/aoc/_2020/day11/Problem.java
+++ src/main/java/io/aoc/_2020/day11/Problem.java
@@ -5,12 +5,21 @@ import io.aoc.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Problem {
     public static final Logger logger = LoggerFactory.getLogger(Problem.class);
+
+    private final List<Coordinate> directions = List.of(
+            new Coordinate(-1, 0),
+            new Coordinate(1, 0),
+            new Coordinate(0, -1),
+            new Coordinate(-1, -1),
+            new Coordinate(0, 1),
+            new Coordinate(-1, 1),
+            new Coordinate(1, -1),
+            new Coordinate(1, 1)
+    );
 
     public static void main(String[] args) {
         var problem = new Problem();
@@ -32,32 +41,18 @@ public class Problem {
         return countOccupiedSeats(result.grid());
     }
 
-    private int part2(String input) {
-        return -1;
-    }
+    private long part2(String input) {
+        var grid = parseInput(input);
 
-    private long countOccupiedSeats(Map<Coordinate, String> grid) {
-        return grid.values().stream()
-                .filter(value -> value.equals("#"))
-                .count();
-    }
+        var result = runPart2(grid);
+        var nrOfChanges = Integer.MAX_VALUE;
 
-
-    private void printGrid(Map<Coordinate, String> grid) {
-        var n = 10;
-        var toPrintGrid = new String[n][n];
-
-        for(var entry : grid.entrySet()) {
-            var coordinate = entry.getKey();
-            toPrintGrid[coordinate.x()][coordinate.y()] = entry.getValue();
+        while (nrOfChanges > 0) {
+            result = runPart2(result.grid());
+            nrOfChanges = result.nrOfChanges();
         }
 
-        for (var i = 0; i< n; i++) {
-            for (var j= 0; j < n; j++) {
-                System.out.print(toPrintGrid[i][j]);
-            }
-            System.out.println();
-        }
+        return countOccupiedSeats(result.grid());
     }
 
     private Result run(Map<Coordinate, String> grid) {
@@ -69,7 +64,7 @@ public class Problem {
             var coordinate = entry.getKey();
 
             if (value.equals("L")) {
-                var occupiedSeatsAround = getOccupiedSeatsAround(grid, coordinate);
+                var occupiedSeatsAround = getOccupiedSeatsAroundPart1(grid, coordinate);
                 if (occupiedSeatsAround == 0) {
                     tmpGrid.put(coordinate, "#");
                     nrOfSeatChanged++;
@@ -77,7 +72,7 @@ public class Problem {
                 }
             }
             if (value.equals("#")) {
-                var occupiedSeatsAround = getOccupiedSeatsAround(grid, coordinate);
+                var occupiedSeatsAround = getOccupiedSeatsAroundPart1(grid, coordinate);
                 if (occupiedSeatsAround >= 4) {
                     tmpGrid.put(coordinate, "L");
                     nrOfSeatChanged++;
@@ -92,7 +87,49 @@ public class Problem {
         return new Result(tmpGrid, nrOfSeatChanged);
     }
 
-    private long getOccupiedSeatsAround(Map<Coordinate, String> grid, Coordinate coordinate) {
+    private Result runPart2(Map<Coordinate, String> grid) {
+        final Map<Coordinate, String> tmpGrid = new HashMap<>();
+        var nrOfSeatChanged = 0;
+
+
+        var lengthX = grid.keySet().stream().map(Coordinate::x).max(Comparator.naturalOrder()).get() + 1;
+        var lengthY = grid.keySet().stream().map(Coordinate::y).max(Comparator.naturalOrder()).get() + 1;
+
+        for (var entry : grid.entrySet()) {
+            var value = entry.getValue();
+            var coordinate = entry.getKey();
+
+            if (value.equals("L")) {
+                var occupiedSeatsAround = getOccupiedSeatsPart2(coordinate, grid, lengthX, lengthY);
+                if (occupiedSeatsAround == 0) {
+                    tmpGrid.put(coordinate, "#");
+                    nrOfSeatChanged++;
+                    continue;
+                }
+            }
+            if (value.equals("#")) {
+                var occupiedSeatsAround = getOccupiedSeatsPart2(coordinate, grid, lengthX, lengthY);
+                if (occupiedSeatsAround >= 5) {
+                    tmpGrid.put(coordinate, "L");
+                    nrOfSeatChanged++;
+                    continue;
+                }
+            }
+
+            tmpGrid.put(coordinate, entry.getValue());
+
+        }
+
+        return new Result(tmpGrid, nrOfSeatChanged);
+    }
+
+    private long countOccupiedSeats(Map<Coordinate, String> grid) {
+        return grid.values().stream()
+                .filter(value -> value.equals("#"))
+                .count();
+    }
+
+    private long getOccupiedSeatsAroundPart1(Map<Coordinate, String> grid, Coordinate coordinate) {
         return neighbours(coordinate).stream()
                 .filter(grid::containsKey)
                 .map(grid::get)
@@ -101,16 +138,37 @@ public class Problem {
     }
 
     private List<Coordinate> neighbours(Coordinate coordinate) {
-        return List.of(
-                new Coordinate(coordinate.x() - 1, coordinate.y()),
-                new Coordinate(coordinate.x() - 1, coordinate.y() - 1),
-                new Coordinate(coordinate.x(), coordinate.y() - 1),
-                new Coordinate(coordinate.x() + 1, coordinate.y() - 1),
-                new Coordinate(coordinate.x() + 1, coordinate.y()),
-                new Coordinate(coordinate.x() + 1, coordinate.y() + 1),
-                new Coordinate(coordinate.x(), coordinate.y() + 1),
-                new Coordinate(coordinate.x() - 1, coordinate.y() + 1)
-        );
+        return directions.stream()
+                .map(direction -> new Coordinate(
+                        coordinate.x() + direction.x(),
+                        coordinate.y() + direction.y())
+                ).toList();
+    }
+
+    private int getOccupiedSeatsPart2(Coordinate coordinate, Map<Coordinate, String> grid, int lengthX, int lengthY) {
+        var occupiedSeats = 0;
+
+        for (var direction : directions) {
+            if (checkFirstSeat(coordinate, direction, grid, lengthX, lengthY)) {
+                occupiedSeats++;
+            }
+        }
+
+        return occupiedSeats;
+    }
+
+    private static boolean checkFirstSeat(Coordinate coordinate, Coordinate direction, Map<Coordinate, String> grid, int lengthX, int lengthY) {
+        var coordinateIterator = new CoordinateIterator(coordinate, direction.x(), direction.y(), lengthX, lengthY);
+        while (coordinateIterator.hasNext()) {
+            var nextCoordinate = coordinateIterator.next();
+            if (grid.get(nextCoordinate).equals("L")) {
+                break;
+            }
+            if (grid.get(nextCoordinate).equals("#")) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private HashMap<Coordinate, String> parseInput(String input) {
@@ -132,4 +190,36 @@ public class Problem {
 record Coordinate(int x, int y) {
 }
 
-record Result(Map<Coordinate, String> grid, int nrOfChanges) {}
+record Result(Map<Coordinate, String> grid, int nrOfChanges) {
+}
+
+class CoordinateIterator implements Iterator<Coordinate> {
+    private final int xDrift;
+    private final int yDrift;
+    private final int xMaxSize;
+    private final int yMaxSize;
+    private Coordinate coordinate;
+
+
+    public CoordinateIterator(Coordinate coordinate, int xDrift, int yDrift, int xMaxSize, int yMaxSize) {
+        this.coordinate = coordinate;
+        this.xDrift = xDrift;
+        this.yDrift = yDrift;
+        this.xMaxSize = xMaxSize;
+        this.yMaxSize = yMaxSize;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return (coordinate.x() + xDrift) >= 0 &&
+                (coordinate.x() + xDrift) < xMaxSize &&
+                (coordinate.y() + yDrift) >= 0 &&
+                (coordinate.y() + yDrift) < yMaxSize;
+    }
+
+    @Override
+    public Coordinate next() {
+        coordinate = new Coordinate(coordinate.x() + xDrift, coordinate.y() + yDrift);
+        return coordinate;
+    }
+}
