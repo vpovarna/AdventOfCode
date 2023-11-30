5d499a1cc285578feb9379b6b4962ce30591e456
vpovarna <vlad.povarna@yahoo.com>
2023-11-30 22:27:32 +0200

    AoC2020, Day12 & Day13, Part1 & Part2 solutions (#9)
    * AoC2020 day12 part1
    
    * AoC2020, Day12, Part1 solution
    
    * AoC2020, Day12, Part1 && Part2 solution
    
    * AoC2020, Day13 Part1 & Part2 solutions
    
    ---------
    
    Co-authored-by: povarna <vlad.povarna@uipath.com>

---
+++ src/main/java/io/aoc/_2020/day12/Problem.java
@@ -0,0 +1,136 @@
+package io.aoc._2020.day12;
+
+import io.aoc.utils.Constants;
+import io.aoc.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Problem {
+    public static final Logger logger = LoggerFactory.getLogger(Problem.class);
+
+    private static final int NORTH = 0;
+    private static final int EAST = 90;
+    private static final int SOUTH = 180;
+    private static final int WEST = 270;
+
+
+    public static void main(String[] args) {
+        var problem = new Problem();
+        var input = Utils.readInputFileAsString(2020, 12, "input.txt");
+        logger.info("Aoc2020, Day10 Problem, Part1: {}", problem.part1(input));
+        logger.info("Aoc2020, Day10 Problem, Part2: {}", problem.part2(input));
+    }
+
+    private int part1(String input) {
+        var instructions = parseInput(input);
+        var shipPosition = new Position(new Coordinate(0, 0), 90);
+
+        for (var instruction : instructions) {
+            var value = instruction.steps();
+            var currentCoordinate = shipPosition.coordinate;
+            switch (instruction.orientation()) {
+                case 'N' -> currentCoordinate.y -= value;
+                case 'S' -> currentCoordinate.y += value;
+                case 'E' -> currentCoordinate.x += value;
+                case 'W' -> currentCoordinate.x -= value;
+                case 'F' -> {
+                    switch (shipPosition.direction) {
+                        case NORTH -> currentCoordinate.y -= value;
+                        case SOUTH -> currentCoordinate.y += value;
+                        case EAST -> currentCoordinate.x += value;
+                        case WEST -> currentCoordinate.x -= value;
+                    }
+                }
+                case 'R' -> shipPosition.direction = changeDirectionPart1(value, shipPosition.direction);
+                case 'L' -> shipPosition.direction = changeDirectionPart1(-value, shipPosition.direction);
+            }
+        }
+
+        return Math.abs(shipPosition.coordinate.x) + Math.abs(shipPosition.coordinate.y);
+
+    }
+
+    private int changeDirectionPart1(int value, int direction) {
+        direction += value;
+        if (direction < 0) {
+            direction += 360;
+        }
+        if (direction >= 360) {
+            direction -= 360;
+        }
+        return direction;
+    }
+
+    private int part2(String input) {
+        var instructions = parseInput(input);
+
+        var shipCoordinate = new Coordinate(0, 0);
+        var viewPointCoordinate = new Coordinate(10, -1);
+
+        for (var instruction : instructions) {
+            var value = instruction.steps();
+            switch (instruction.orientation()) {
+                case 'N' -> viewPointCoordinate.y -= value;
+                case 'S' -> viewPointCoordinate.y += value;
+                case 'E' -> viewPointCoordinate.x += value;
+                case 'W' -> viewPointCoordinate.x -= value;
+                case 'F' -> {
+                    shipCoordinate.x += viewPointCoordinate.x * value;
+                    shipCoordinate.y += viewPointCoordinate.y * value;
+                }
+                case 'L' -> viewPointCoordinate = changeDirectionPart2(-value, viewPointCoordinate);
+                case 'R' -> viewPointCoordinate = changeDirectionPart2(value, viewPointCoordinate);
+            }
+        }
+
+        return Math.abs(shipCoordinate.x) + Math.abs(shipCoordinate.y);
+    }
+
+    private static Coordinate changeDirectionPart2(int value, Coordinate viewPointCoordinate) {
+        if (value < 0) {
+            value += 360;
+        }
+
+        double radians = Math.toRadians(value);
+
+        float newx = (float) (viewPointCoordinate.x * Math.cos(radians) - viewPointCoordinate.y * Math.sin(radians));
+        float newy = (float) (viewPointCoordinate.x * Math.sin(radians) + viewPointCoordinate.y * Math.cos(radians));
+
+        return new Coordinate((int) newx, (int) newy);
+    }
+
+    private List<Instruction> parseInput(String input) {
+        var lines = input.split(Constants.EOL);
+        return Arrays.stream(lines)
+                .map(String::trim)
+                .map(line -> new Instruction(line.charAt(0), Integer.parseInt(line.substring(1))))
+                .toList();
+    }
+
+}
+
+class Coordinate {
+    int x;
+    int y;
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+record Instruction(char orientation, int steps) {
+}
+
+class Position {
+    Coordinate coordinate;
+    int direction;
+
+    public Position(Coordinate coordinate, int direction) {
+        this.coordinate = coordinate;
+        this.direction = direction;
+    }
+}
---
+++ src/main/java/io/aoc/_2020/day13/Problem.java
@@ -0,0 +1,100 @@
+package io.aoc._2020.day13;
+
+import io.aoc.utils.Constants;
+import io.aoc.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Problem {
+
+    private final static Logger logger = LoggerFactory.getLogger(Problem.class);
+
+    public static void main(String[] args) {
+        var problem = new Problem();
+        var input = Utils.readInputFileAsString(2020, 13, "input.txt");
+        logger.info("Aoc2020, Day10 Problem, Part1: {}", problem.part1(input));
+        logger.info("Aoc2020, Day10 Problem, Part2: {}", problem.part2(input));
+    }
+
+    private int part1(String input) {
+        var schedule = parseInputPart1(input);
+        int timeToDepart = schedule.timeToDepart();
+        System.out.println(timeToDepart);
+        List<Integer> busses = schedule.busses();
+        System.out.println(busses);
+
+        var firstBussToDepartNumber = 0;
+        var earliestTimeToDepart = Integer.MAX_VALUE;
+
+        for (var bussNr : busses) {
+            var n = getEarliestTimeToDepart(bussNr, timeToDepart);
+            var bussTimeToDepart = n * bussNr;
+            if (bussTimeToDepart < earliestTimeToDepart) {
+                firstBussToDepartNumber = bussNr;
+                earliestTimeToDepart = bussTimeToDepart;
+            }
+        }
+        return firstBussToDepartNumber * (earliestTimeToDepart - timeToDepart);
+    }
+
+    /**
+     * The common number of two prime numbers is equal to the product of them.
+     *   Ex: 3, 7 have the common number = 21
+     *
+     *  The tine will only increase with the stepSize, as long as the (time + index) % buss != 0
+     *  https://www.youtube.com/watch?v=4_5mluiXF5I&ab_channel=AlvintheProgrammer
+     */
+    private long part2(String input) {
+        var busses = parseInputPart2(input);
+        long time = 0L;
+        long stepSize = busses.get(0);
+
+        for (var i = 1; i < busses.size(); i++) {
+            var buss = busses.get(i);
+
+            while ((time + i) % buss != 0) {
+                time += stepSize;
+            }
+
+            stepSize *= buss;
+        }
+
+
+        return time;
+    }
+
+    private int getEarliestTimeToDepart(int bussNr, int timeToDepart) {
+        return (timeToDepart / bussNr) + 1;
+    }
+
+    private Schedule parseInputPart1(String input) {
+        var lines = input.split(Constants.EOL);
+        if (lines.length != 2) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+
+        var earliestTimeToDepart = Integer.parseInt(lines[0]);
+        var busses = Arrays.stream(lines[1].split(","))
+                .filter(x -> !x.equals("x"))
+                .map(Integer::parseInt)
+                .toList();
+
+        return new Schedule(earliestTimeToDepart, busses);
+    }
+
+    private List<Integer> parseInputPart2(String input) {
+        var lines = input.split(Constants.EOL);
+        if (lines.length != 2) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        return Arrays.stream(lines[1].split(","))
+                .map(x -> (x.equals("x")) ? 1 : Integer.parseInt(x))
+                .toList();
+    }
+}
+
+record Schedule(int timeToDepart, java.util.List<Integer> busses) {
+}
