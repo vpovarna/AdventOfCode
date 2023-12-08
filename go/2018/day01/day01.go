d1c80afb2230c02327c7b17fd55912cd5b6c9817
povarna <vlad.povarna@uipath.com>
2023-12-08 22:21:34 +0200

    AoC2018 Day1, Part1 & Part2 solutions

---
+++ go/2018/day01/day01.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var inputFile = flag.String("inputFile", "day01.input", "Relative file path to use as input")
+
+func main() {
+	flag.Parse()
+	bytes, err := os.ReadFile(*inputFile)
+
+	if err != nil {
+		panic(err)
+	}
+
+	input := string(bytes)
+
+	fmt.Printf("AoC 2018, Day1, Part1 result is: %d \n", part1(&input))
+	fmt.Printf("AoC 2018, Day1, Part2 result is: %d \n", part2(&input))
+}
+
+func part1(input *string) int {
+	result := 0
+	for _, line := range strings.Split(*input, "\n") {
+		offset, _ := strconv.Atoi(line)
+		result += offset
+	}
+
+	return result
+}
+
+func part2(input *string) int {
+	result := 0
+	seen := make(map[int]bool)
+
+	for {
+		for _, line := range strings.Split(*input, "\n") {
+			offset, _ := strconv.Atoi(line)
+			result += offset
+
+			if seen[result] {
+				return result
+			}
+			seen[result] = true
+		}
+	}
+}
