7db782823b27b07f6a4c101f3bb5ccb6df8ca205
vlad.povarna <vlad.povarna@uipath.com>
2023-12-13 20:35:58 +0200

    Wip

--- src/main/java/io/aoc/_2023/day13/Problem.java
+++ src/main/java/io/aoc/_2023/day13/Problem.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.ToIntFunction;
+import java.util.stream.Collectors;
 
 public class Problem {
     private static final Logger logger = LoggerFactory.getLogger(Problem.class);
@@ -25,15 +26,18 @@ public class Problem {
         var lineMultiplicationFactor = 100;
         var grids = parseInput(input);
 
-        ToIntFunction<Grid> count = grid -> lineMultiplicationFactor * getReflectionLineIndex(grid) + getReflectionLineIndex(grid.transpose());
+        ToIntFunction<Grid> count = grid -> lineMultiplicationFactor * (getReflectionLineIndex(grid, -1) + 1) + getReflectionLineIndex(grid.transpose(), -1) + 1;
 
         return grids.stream()
                 .mapToInt(count)
                 .sum();
     }
 
-    private int part2(String inputFile) {
-        return 0;
+    private int part2(String input) {
+        var grids = parseInput(input);
+        return grids.stream()
+                .mapToInt(this::mutateAndGetReflection)
+                .sum();
     }
 
     private List<Grid> parseInput(String input) {
@@ -43,7 +47,9 @@ public class Problem {
     }
 
     private Grid getGrid(String rowGrid) {
-        var lines = Arrays.stream(rowGrid.split(Constants.EOL)).toList();
+        var lines = Arrays
+                .stream(rowGrid.split(Constants.EOL))
+                .collect(Collectors.toList());
         return new Grid(lines);
     }
 
@@ -67,13 +73,56 @@ public class Problem {
         return true;
     }
 
-    private int getReflectionLineIndex(Grid grid) {
+    private int getReflectionLineIndex(Grid grid, int avoidIndex) {
         var gridLines = grid.lines();
 
         int size = gridLines.size();
         for (var i = 0; i < size - 1; i++) {
-            if (hasReflection(gridLines, i)) {
-                return i + 1;
+            if (i != avoidIndex && hasReflection(gridLines, i)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private int mutateAndGetReflection(Grid grid) {
+        var gridLines = grid.lines();
+
+        var origHorizontalLineNr = getReflectionLineIndex(grid, -1);
+        var origVerticalLineNr = getReflectionLineIndex(grid.transpose(), -1);
+
+
+        for (var i = 0; i < gridLines.size(); i++) {
+            var currentLine = gridLines.get(i).toCharArray();
+            for (var j = 0; j < currentLine.length; j++) {
+                if (gridLines.get(i).charAt(j) == '#') {
+                    currentLine[j] = '.';
+                } else {
+                    currentLine[j] = '#';
+                }
+                gridLines.set(i, String.valueOf(currentLine));
+
+                var newGrid = new Grid(gridLines);
+                var newHorizontalLineNr = getReflectionLineIndex(newGrid, origHorizontalLineNr);
+                var newVerticalLineNr = getReflectionLineIndex(newGrid.transpose(), origVerticalLineNr);
+
+                newGrid.lines().forEach(System.out::println);
+                System.out.println("-------------------------");
+//                System.out.println(i + " " + j);
+//                System.out.println("(" + origHorizontalLineNr + "," + origVerticalLineNr + ")");
+//                System.out.println("(" + newHorizontalLineNr + "," + newVerticalLineNr + ")");
+
+//                var result = 0;
+
+//                   if ((newHorizontalLineNr != -1 && newVerticalLineNr != -1) || (newHorizontalLineNr != origHorizontalLineNr && newVerticalLineNr != origVerticalLineNr)) {
+//                    if (newHorizontalLineNr != -1) {
+//                        result += (newHorizontalLineNr + 1) * 100;
+//                    } else {
+//                        result += newVerticalLineNr + 1;
+//                    }
+//                    return result;
+//                }
             }
         }
 
