c088b92f7206871cc3c7eb0ed66f88b22fd0c916
vlad.povarna <vlad.povarna@uipath.com>
2023-12-14 12:07:35 +0200

    AoC2023, Day14 Part2 solution

--- src/main/java/io/aoc/_2023/day14/Problem.java
+++ src/main/java/io/aoc/_2023/day14/Problem.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Problem {
@@ -26,13 +27,14 @@ public class Problem {
         int n = lines[0].length();
         int m = lines.length;
 
+        // Move to North
         for (var i = 0; i < n; i++) {
             for (var j = 0; j < m; j++) {
                 var point = new Point(i, j);
                 var charAt = grid.get(point);
 
                 if (charAt.equals('O')) {
-                    var lastFree = firstFreeSpot(grid, point);
+                    var lastFree = firstFreeSpot(grid, Coordinate.N, point);
                     if (lastFree != null) {
                         grid.put(lastFree, 'O');
                         grid.put(point, '.');
@@ -53,8 +55,103 @@ public class Problem {
         return sum;
     }
 
-    private int part2(String input) {
-        return 0;
+    private long part2(String input) {
+        var lines = input.split(Constants.EOL);
+        var grid = getGrid(lines);
+
+        int n = lines[0].length();
+        int m = lines.length;
+
+        var memory = new HashMap<HashMap<Point, Character>, Integer>();
+        boolean enableMemory = true;
+        int cycles = 1000000000;
+
+        for (var cycle = 1; cycle <= cycles; cycle++) {
+            // Rotate North
+            for (var i = 0; i < n; i++) {
+                for (var j = 0; j < m; j++) {
+                    var point = new Point(i, j);
+                    var charAt = grid.get(point);
+
+                    if (charAt == 'O') {
+                        var lastFree = firstFreeSpot(grid, Coordinate.N, point);
+                        if (lastFree != null) {
+                            grid.put(lastFree, 'O');
+                            grid.put(point, '.');
+                        }
+                    }
+                }
+            }
+
+            // Rotate West
+            for (var i = 0; i < m; i++) {
+                for (var j = 0; j < n; j++) {
+                    var point = new Point(j, i);
+                    var charAt = grid.get(point);
+
+                    if (charAt == 'O') {
+                        var lastFree = firstFreeSpot(grid, Coordinate.W, point);
+                        if (lastFree != null) {
+                            grid.put(lastFree, 'O');
+                            grid.put(point, '.');
+                        }
+                    }
+                }
+            }
+
+            // Rotate South
+            for (var i = 0; i < n; i++) {
+                for (var j = m - 1; j >= 0; j--) {
+                    var point = new Point(i, j);
+                    var charAt = grid.get(point);
+
+                    if (charAt == 'O') {
+                        var lastFree = firstFreeSpot(grid, Coordinate.S, point);
+                        if (lastFree != null) {
+                            grid.put(lastFree, 'O');
+                            grid.put(point, '.');
+                        }
+                    }
+                }
+            }
+
+            // East tilt
+            for (var i = 0; i < m; i++) {
+                for (var j = n - 1; j >= 0; j--) {
+                    var point = new Point(j, i);
+                    var charAt = grid.get(point);
+
+                    if (charAt == 'O') {
+                        var lastFree = firstFreeSpot(grid, Coordinate.E, point);
+                        if (lastFree != null) {
+                            grid.put(lastFree, 'O');
+                            grid.put(point, '.');
+                        }
+                    }
+                }
+            }
+
+            if (enableMemory) {
+                if (memory.containsKey(grid)) {
+                    var firstSeenIndex = memory.get(grid);
+                    var repeatingWindow = cycle - firstSeenIndex;
+                    cycle = cycles - ((cycles - firstSeenIndex) % repeatingWindow);
+                    enableMemory = false;
+                } else {
+                    memory.put(new HashMap<>(grid), cycle);
+                }
+            }
+        }
+
+        var sum = 0L;
+
+        for (var a : grid.entrySet()) {
+            if (a.getValue() == 'O') {
+                sum += m - a.getKey().y();
+            }
+        }
+
+        return sum;
     }
 
     private Map<Point, Character> getGrid(String[] lines) {
@@ -71,12 +168,17 @@ public class Problem {
         return grid;
     }
 
-    private Point firstFreeSpot(Map<Point, Character> grid, Point point) {
+    private Point firstFreeSpot(Map<Point, Character> grid, Coordinate direction, Point point) {
         Point lastFree = null;
         var previousPoint = point;
 
         while (true) {
-            previousPoint = previousPoint.north();
+            previousPoint = switch (direction) {
+                case N -> previousPoint.north();
+                case S -> previousPoint.south();
+                case E -> previousPoint.east();
+                case W -> previousPoint.west();
+            };
             var charAt = grid.getOrDefault(previousPoint, null);
             if (charAt == null || charAt != '.') {
                 break;
@@ -95,9 +197,34 @@ record Point(int x, int y) {
         return new Point(x, y - 1);
     }
 
+    public Point south() {
+        return new Point(x, y + 1);
+    }
+
+    public Point east() {
+        return new Point(x + 1, y);
+    }
+
+    public Point west() {
+        return new Point(x - 1, y);
+    }
+
+    public List<Point> getAdjacentPoints() {
+        return List.of(
+                new Point(x - 1, y),
+                new Point(x + 1, y),
+                new Point(x, y - 1),
+                new Point(x, y + 1)
+        );
+    }
+
+
     @Override
     public String toString() {
         return "[" + x + "," + y + "]";
     }
 }
 
+enum Coordinate {
+    N, W, E, S
+}
