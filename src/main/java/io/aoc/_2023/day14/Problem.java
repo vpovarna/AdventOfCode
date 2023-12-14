46717a10f67944bdd6ff75046b711e63373084bf
vlad.povarna <vlad.povarna@uipath.com>
2023-12-14 09:06:25 +0200

    AoC2023, Day14 Part1 solution

---
+++ src/main/java/io/aoc/_2023/day14/Problem.java
@@ -0,0 +1,103 @@
+package io.aoc._2023.day14;
+
+import io.aoc.utils.Constants;
+import io.aoc.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Problem {
+    private static final Logger logger = LoggerFactory.getLogger(Problem.class);
+
+    public static void main(String[] args) {
+        var inputFile = Utils.readInputFileAsString(2023, 14);
+
+        var problem = new Problem();
+        logger.info("Aoc2023, Day12 Problem, Part1: {}", problem.part1(inputFile));
+        logger.info("Aoc2023, Day12 Problem, Part2: {}", problem.part2(inputFile));
+    }
+
+    private long part1(String input) {
+        var lines = input.split(Constants.EOL);
+        var grid = getGrid(lines);
+
+        int n = lines[0].length();
+        int m = lines.length;
+
+        for (var i = 0; i < n; i++) {
+            for (var j = 0; j < m; j++) {
+                var point = new Point(i, j);
+                var charAt = grid.get(point);
+
+                if (charAt.equals('O')) {
+                    var lastFree = firstFreeSpot(grid, point);
+                    if (lastFree != null) {
+                        grid.put(lastFree, 'O');
+                        grid.put(point, '.');
+                    }
+                }
+            }
+        }
+
+
+        var sum = 0L;
+
+        for (var a : grid.entrySet()) {
+            if (a.getValue() == 'O') {
+                sum += m - a.getKey().y();
+            }
+        }
+
+        return sum;
+    }
+
+    private int part2(String input) {
+        return 0;
+    }
+
+    private Map<Point, Character> getGrid(String[] lines) {
+        var grid = new HashMap<Point, Character>();
+
+        for (var y = 0; y < lines.length; y++) {
+            var line = lines[y];
+
+            for (var x = 0; x < line.length(); x++) {
+                grid.put(new Point(x, y), line.charAt(x));
+            }
+        }
+
+        return grid;
+    }
+
+    private Point firstFreeSpot(Map<Point, Character> grid, Point point) {
+        Point lastFree = null;
+        var previousPoint = point;
+
+        while (true) {
+            previousPoint = previousPoint.north();
+            var charAt = grid.getOrDefault(previousPoint, null);
+            if (charAt == null || charAt != '.') {
+                break;
+            }
+
+            lastFree = previousPoint;
+        }
+
+        return lastFree;
+
+    }
+}
+
+record Point(int x, int y) {
+    public Point north() {
+        return new Point(x, y - 1);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + x + "," + y + "]";
+    }
+}
+
