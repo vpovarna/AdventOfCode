c9e1538a2abb3b555640f2d141298db7e2b2e918
vlad.povarna <vlad.povarna@uipath.com>
2023-12-21 16:44:13 +0200

    AoC2016, day04 part2 solution

--- go/2016/day04/day04.go
+++ go/2016/day04/day04.go
@@ -11,6 +11,7 @@ import (
 )
 
 type room struct {
+	name       string
 	roomNumber int
 	checksum   string
 	occurrence map[rune]int
@@ -44,6 +45,24 @@ func (r *room) getObservedChars(size int) []freqEntry {
 	return observed
 }
 
+func (r *room) decryptRoomName() int {
+	name := r.name
+	roomNumber := r.roomNumber
+
+	var sb strings.Builder
+
+	for _, c := range name {
+		if c == '-' {
+			sb.WriteRune(' ')
+		}
+		sb.WriteRune(rune('a' + (int(c)-'a'+roomNumber)%26))
+		if sb.String() == "northpole" {
+			return roomNumber
+		}
+	}
+	return -1
+}
+
 var inputFile = flag.String("inputFile", "input.txt", "Relative file path to use as input")
 
 func main() {
@@ -56,8 +75,8 @@ func main() {
 
 	input := string(bytes)
 
-	fmt.Printf("AoC 2016, Day3, Part1 solution is: %d \n", part1(&input))
-	fmt.Printf("AoC 2016, Day3, Part2 solution is: %d \n", part2(&input))
+	fmt.Printf("AoC 2016, Day4, Part1 solution is: %d \n", part1(&input))
+	fmt.Printf("AoC 2016, Day4, Part2 solution is: %d \n", part2(&input))
 }
 
 func part1(input *string) int {
@@ -84,6 +103,14 @@ func part1(input *string) int {
 }
 
 func part2(input *string) int {
+	lines := strings.Split(*input, "\n")
+	for _, line := range lines {
+		room := getRoom(line)
+		nr := room.decryptRoomName()
+		if (nr) > -1 {
+			return nr
+		}
+	}
 	return -1
 }
 
@@ -110,6 +137,7 @@ func getRoom(row string) room {
 	}
 
 	return room{
+		name:       name,
 		roomNumber: id,
 		occurrence: occurrence,
 		checksum:   check,
