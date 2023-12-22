aaa54a3b1ab3850eed6931c968c21e40bc128e04
vlad.povarna <vlad.povarna@uipath.com>
2023-12-22 17:52:59 +0200

    AoC2016 Day05 Part2 solution

--- go/2016/day05/day05.go
+++ go/2016/day05/day05.go
@@ -20,6 +20,7 @@ func main() {
 
 	input := string(bytes)
 	fmt.Printf("AoC 2016, Day5, Part1 solution is: %s \n", part1(&input))
+	fmt.Printf("AoC 2016, Day5, Part2 solution is: %s \n", part2(&input))
 }
 
 func part1(input *string) string {
@@ -38,6 +39,38 @@ func part1(input *string) string {
 		index += 1
 	}
 	return strings.Join(password, "")
+}
+
+func part2(input *string) string {
+	index := 1
+	cap := 8
+
+	var password [8]string
+
+	for {
+		s := fmt.Sprintf("%s%d", *input, index)
+		md5 := getMD5Hash(s)
+		if md5[0:5] == "00000" {
+			// The the int value by converting the rune to int and substract 0
+			i := int(md5[5]) - '0'
+			
+			// If the idex is greater then the max idex or if we index is already field, move fw
+			if i > 7 || password[i] != "" {
+				index += 1
+				continue
+			}
+
+			password[i] = string(md5[6])
+			cap -= 1
+
+			if cap == 0 {
+				break
+			}
+		}
+		index += 1
+	}
+
+	return strings.Join(password[:], "")
 }
 
 func getMD5Hash(text string) string {
