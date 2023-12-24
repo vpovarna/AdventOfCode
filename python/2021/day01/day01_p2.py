00b80be3e5eded97cf3aa56b446c4d43b34a5016
povarna <vlad.povarna@uipath.com>
2023-12-24 16:06:07 +0200

    AoC 2021 day 1 solutions

--- python/2021/day01/day01_p2.py
+++ python/2021/day01/day01_p2.py
@@ -6,4 +6,6 @@ file = open(sys.argv[1], encoding='utf-8')
 lines = file.readlines()
 values = [*map(int, lines)]
 
-print (sum( y > x for x, y in zip(values, values[1:])))
+r = [x + y + z for x, y, z in zip(values, values[1:], values[2:])]
+
+print(sum(y > x for x, y in zip(r, r[1:])))
