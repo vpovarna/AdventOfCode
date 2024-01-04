648447953895500363e17f7e36227cf7b0eafd79
povarna <vlad.povarna@uipath.com>
2024-01-04 15:01:28 +0200

    AoC 2016 Day13, Part1 solution

--- go/2016/day13/day13.go
+++ go/2016/day13/day13.go
@@ -9,30 +9,11 @@ import (
 
 var inputFile = flag.String("inputFile", "input.txt", "Relative file path to use as input")
 
-type Point struct {
-	x int
-	y int
-}
-
-func (p *Point) getNeighbors() []Point {
-	return []Point{
-		{p.x + 1, p.y},
-		{p.x - 1, p.y},
-		{p.x, p.y - 1},
-		{p.x, p.y + 1},
-	}
-}
-
-func (p *Point) checkSpace(favNumber int) bool {
-	sum := int64((p.x*p.x + 3*p.x + 2*p.x*p.y + p.y + p.y*p.y) + favNumber)
-	binaryRep := strconv.FormatInt(sum, 2)
-
-	occurrence := map[rune]int{}
-
-	for _, c := range binaryRep {
-		occurrence[c] += 1
-	}
-	return occurrence[rune('1')]%2 == 0
+var dirs = [][2]int{
+	{-1, 0},
+	{1, 0},
+	{0, -1},
+	{0, 1},
 }
 
 func main() {
@@ -56,26 +37,58 @@ func part1(input *string) int {
 		panic("unable to convert the input to int")
 	}
 
-	// draw grid
-	for i := 0; i < 7; i++ {
-		for j := 0; j < 10; j++ {
-			p := Point{i, j}
-			if p.checkSpace(favNumber) {
-				fmt.Print(".")
-			} else {
-				fmt.Print("#")
-			}
-		}
-		fmt.Println()
-	}
-
-	// fmt.Println(favNumber)
-	// p := Point{5, 0}
-	// fmt.Println(p.checkSpace(favNumber))
-
-	return -1
+	return bfs(favNumber, [2]int{31, 39})
 }
 
 func part2(input *string) int {
 	return -1
+}
+
+func bfs(favNumber int, destination [2]int) int {
+	// Queue represented as a slice of arrays
+	queue := [][3]int{[3]int{1, 1, 0}} // starting pint (x, y, distance)
+
+	// HashSet to track the visited coordinates
+	visited := map[[2]int]bool{}
+
+	for len(queue) > 0 {
+		front := queue[0]
+
+		currentX, currentY := front[0], front[1]
+		currentDist := front[2]
+
+		if currentX == destination[0] && currentY == destination[1] {
+			return currentDist
+		}
+
+		// if not visited
+		if !visited[[2]int{currentX, currentY}] {
+			for _, dir := range dirs {
+				nextX, nextY := dir[0]+currentX, dir[1]+currentY
+				if nextX >= 0 && nextY >= 0 {
+					if isOpen(nextX, nextY, favNumber) {
+						queue = append(queue, [3]int{nextX, nextY, currentDist + 1})
+					}
+				}
+			}
+		}
+		visited[[2]int{currentX, currentY}] = true
+
+		// Update queue
+		queue = queue[1:]
+	}
+
+	return -1
+}
+
+func isOpen(x, y int, favNumber int) bool {
+	sum := int64((x*x + 3*x + 2*x*y + y + y*y) + favNumber)
+	binaryRep := strconv.FormatInt(sum, 2)
+
+	occurrence := map[rune]int{}
+
+	for _, c := range binaryRep {
+		occurrence[c] += 1
+	}
+	return occurrence[rune('1')]%2 == 0
 }
