077b5a84dce1f296cbd1f47d01465799bfa44482
povarna <vlad.povarna@uipath.com>
2024-01-04 22:30:26 +0200

    AoC2015 Day01 Part1 and Part2 solutions

--- go/2015/day01/day01.go
+++ go/2015/day01/day01.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var inputFile = flag.String("inputFile", "input.txt", "Relative file path to use as input")
 
 func main() {
+	flag.Parse()
+	bytes, err := os.ReadFile(*inputFile)
+
+	if err != nil {
+		panic(err)
+	}
+
+	input := string(bytes)
+	fmt.Printf("AoC 2015, Day1, Part1 solution is: %d \n", part1(&input))
+	fmt.Printf("AoC 2015, Day1, Part2 solution is: %d \n", part2(&input))
+
 	fmt.Println("Welcome to AOC 2015!")
+}
+
+func part1(input *string) int {
+	count := 0
+
+	for _, c := range *input {
+		if c == '(' {
+			count += 1
+		} else if c == ')' {
+			count -= 1
+		} else {
+			panic("unknown character")
+		}
+	}
+
+	return count
+}
+
+func part2(input *string) int {
+	count := 0
+
+	for i, c := range *input {
+		if count == -1 {
+			return i
+		}
+		if c == '(' {
+			count += 1
+		} else if c == ')' {
+			count -= 1
+		} else {
+			panic("unknown character")
+		}
+	}
+
+	return -1
 }
