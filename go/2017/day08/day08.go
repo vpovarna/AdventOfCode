45dae661e971f59ae56f5c05652d28bf8fe12170
povarna <vlad.povarna@uipath.com>
2024-01-15 19:47:30 +0200

    AOC 2017 Day08 WIP

--- go/2017/day08/day08.go
+++ go/2017/day08/day08.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var inputFile = flag.String("inputFile", "input.txt", "Relative file path to use as input")
@@ -22,6 +23,73 @@ func main() {
 }
 
 func part1(input string) int {
+	instructions := strings.Split(input, "\n")
+	registry := map[string]int{}
+
+	for _, instruction := range instructions {
+		var t, v, op1, op2 string
+		var n1, n2 int
+
+		fmt.Sscanf(instruction, "%s %s %d if %s %s %d", &t, &op1, &n1, &v, &op2, &n2)
+
+		// populate registry
+		for _, c := range [2]string{t, v} {
+			if _, contains := registry[c]; !contains {
+				registry[c] = 0
+			}
+		}
+
+		fmt.Println(instruction)
+		fmt.Println(op2)
+		switch op2 {
+		case ">":
+			if registry[v] > n2 {
+				if op1 == "inc" {
+					registry[t] += n1
+				} else {
+					registry[t] -= n1
+				}
+			}
+		case "<":
+			if registry[v] < n2 {
+				if op1 == "inc" {
+					registry[t] += n1
+				} else {
+					registry[t] -= n1
+				}
+			}
+		case "==":
+			if registry[v] == n2 {
+				if op1 == "inc" {
+					registry[t] += n1
+				} else {
+					registry[t] -= n1
+				}
+			}
+		case ">=":
+			if registry[v] >= n2 {
+				if op1 == "inc" {
+					registry[t] += n1
+				} else {
+					registry[t] -= n1
+				}
+			}
+		case "<=":
+			if registry[v] <= n2 {
+				if op1 == "inc" {
+					registry[t] += n1
+				} else {
+					registry[t] -= n1
+				}
+			}
+		default:
+			panic("invalid operation")
+		}
+		fmt.Println(registry)
+		fmt.Println("=====")
+	}
+	fmt.Println(registry)
+
 	return -1
 }
 
