f86c374af9fdf943d16cdabbe4fa6b2907bdacb0
povarna <vlad.povarna@uipath.com>
2024-01-16 22:00:51 +0200

    AoC 2017 day12 part1 solution

--- go/2017/day12/day12.go
+++ go/2017/day12/day12.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"aoc/cast"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var inputFile = flag.String("inputFile", "input.txt", "Relative file path to use as input")
@@ -22,9 +24,47 @@ func main() {
 }
 
 func part1(input string) int {
-	return -1
+	graph := buildGraph(input)
+
+	var count int
+	for k := range graph {
+		if dfs(graph, k, 0, map[int]bool{}) {
+			count++
+		}
+	}
+	return count
 }
 
 func part2(input string) int {
 	return -1
+}
+
+func buildGraph(input string) map[int][]int {
+	lines := strings.Split(input, "\n")
+	graph := make(map[int][]int, 0)
+	for _, line := range lines {
+		parts := strings.Split(line, " <-> ")
+		id := cast.ToInt(parts[0])
+		for _, child := range strings.Split(parts[1], ", ") {
+			graph[id] = append(graph[id], cast.ToInt(child))
+		}
+	}
+
+	return graph
+}
+
+func dfs(graph map[int][]int, entry int, target int, visited map[int]bool) bool {
+	if visited[entry] {
+		return false
+	}
+
+	visited[entry] = true
+
+	for _, child := range graph[entry] {
+		if child == target || dfs(graph, child, target, visited) {
+			return true
+		}
+	}
+
+	return false
 }
