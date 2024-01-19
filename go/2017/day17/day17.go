d0ee441512ee08b029c250159a20f2269c640aa0
povarna <vlad.povarna@uipath.com>
2024-01-19 09:39:39 +0200

    AoC2017, Part1 and Part2 solution

--- go/2017/day17/day17.go
+++ go/2017/day17/day17.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"aoc/cast"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 )
 
 var inputFile = flag.String("inputFile", "input.txt", "Relative file path to use as input")
+
+type llNode struct {
+	value int
+	next  *llNode
+}
 
 func main() {
 	flag.Parse()
@@ -23,9 +30,40 @@ func main() {
 }
 
 func part1(input string) int {
-	return -1
+	return spinLock(input, 2017, 2017)
 }
 
 func part2(input string) int {
-	return -1
+	return spinLock(input, 50000000, 0)
+}
+
+func spinLock(input string, lastNumToAdd int, valueToFind int) int {
+	steps := cast.ToInt(input)
+
+	current := &llNode{value: 0}
+	current.next = current
+	for i := 1; i <= lastNumToAdd; i++ {
+		for j := 0; j < steps; j++ {
+			current = current.next
+		}
+
+		saveNext := current.next
+		current.next = &llNode{
+			value: i,
+			next:  saveNext,
+		}
+		current = current.next
+
+		// Monitor progress for part2
+		if i%1000000 == 0 {
+			log.Println(i, "steps done")
+		}
+	}
+
+	// find the valueToInt value
+	for current.value != valueToFind {
+		current = current.next
+	}
+
+	return current.next.value
 }
