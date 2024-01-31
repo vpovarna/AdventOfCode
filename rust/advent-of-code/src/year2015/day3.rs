f3744fc83e405fb07e2b111e1f274c73b3f685e9
povarna <vlad.povarna@uipath.com>
2024-01-31 08:36:15 +0200

    AoC 2015 WIP

---
+++ rust/advent-of-code/src/year2015/day3.rs
@@ -0,0 +1,13 @@
+pub fn solve() {
+    let input_file_path = String::from("src/year2015/resources/day3.txt");
+    println!("AoC 2015, Day3 Part1 solution is: {}", part1(&input_file_path));
+    println!("AoC 2015, Day3 Part2 solution is: {}", part1(&input_file_path));
+}
+
+fn part1(input: &String) -> i32 {
+    -1
+}
+
+fn part2(input: &String) -> i32 {
+    -1
+}
--- rust/advent-of-code/src/year2015/mod.rs
+++ rust/advent-of-code/src/year2015/mod.rs
@@ -1,3 +1,4 @@
 pub mod day1;
 pub mod day2;
 pub mod day2_v2;
+pub mod day3;
