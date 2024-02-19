b460ac281b61459eda683131485d02363f561ac8
povarna <vlad.povarna@uipath.com>
2024-02-19 13:37:25 +0200

    AoC 2022 Day10 solution

--- go/2022/day10/day10.go
+++ go/2022/day10/day10.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"aoc/cast"
+	"aoc/mathy"
 	"flag"
 	"fmt"
 	"os"
@@ -20,7 +21,8 @@ func main() {
 	input := string(bytes)
 
 	fmt.Printf("AoC 2022, Day10, Part1 solution is: %d \n", part1(input))
-	fmt.Printf("AoC 2022, Day10, Part2 solution is: %d \n", part2(input))
+	fmt.Printf("AoC 2022, Day10, Part2 solution is:\n")
+	part2(input)
 }
 
 func part1(input string) int {
@@ -58,6 +60,37 @@ func part1(input string) int {
 	return ans
 }
 
-func part2(input string) int {
-	return -1
+func part2(input string) {
+	x := 1
+
+	output := []int{}
+	lines := strings.Split(input, "\n")
+
+	for _, line := range lines {
+		if line == "noop" {
+			output = append(output, x)
+		} else {
+			parts := strings.Split(line, " ")
+			count := cast.ToInt(parts[1])
+
+			// wait to cycles
+			output = append(output, x)
+			output = append(output, x)
+			x += count
+		}
+	}
+
+	var draw = strings.Builder{}
+
+	for i := 0; i < len(output); i += 40 {
+		for j := 0; j < 40; j++ {
+			if mathy.AbsInt(output[i+j]-j) <= 1 {
+				draw.WriteString("#")
+			} else {
+				draw.WriteString(" ")
+			}
+		}
+		draw.WriteString("\n")
+	}
+	fmt.Println(draw.String())
 }
