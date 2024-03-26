5fb9bb553e90f4f39e776a303d46d285ba65caae
povarna <vlad.povarna@uipath.com>
2024-03-26 22:38:30 +0200

    AoC2022 Python day01 year 2022 solution

---
+++ python/2022/day01/day01.py
@@ -0,0 +1,52 @@
+import argparse
+from typing import List
+
+
+def part1(input) -> int:
+    loads = get_calories(input)
+    return max(loads)
+
+
+def part2(input: str) -> int:
+    loads = get_calories(input)
+    loads.sort()
+    return sum(loads[-3:])
+
+
+def parse_input(input: str) -> List[str]:
+    with open(input) as fn:
+        raw_data = fn.read().strip()
+        parts = raw_data.split("\n\n")
+    return parts
+
+
+def get_calories(input: str) -> List[int]:
+    loads = []
+    for part in parse_input(input):
+        calories = [
+            int(x) for x in part.split()
+        ]  # this can be written as: map(int, part.split())
+        loads.append(sum(calories))
+    return loads
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-t",
+        "--test_input",
+        help="Run Program with test input",
+        type=bool,
+        default=False,
+    )
+
+    args = parser.parse_args()
+
+    input_file = "test_input.txt" if args.test_input else "input.txt"
+
+    print(f"AoC2022 Day1, Part1 solution is: {part1(input_file)}")
+    print(f"AoC2022 Day1, Part2 solution is: {part2(input_file)}")
+
+
+if __name__ == "__main__":
+    main()
