bdd5f7a1b967b6f0159e40de6f16edd44a84cf9e
povarna <vlad.povarna@uipath.com>
2024-04-02 14:24:09 +0300

    AoC2022 Day04 Part1 and Part2 solutions

---
+++ python/2022/day04/day04.py
@@ -0,0 +1,59 @@
+from typing import Callable, List
+
+
+def run(input: str, fn: Callable[[List[int], List[int]], bool]) -> int:
+    count = 0
+    for line in parse_input(input):
+        parts = line.split(",")
+        assert len(parts) == 2
+
+        elf1 = [int(x) for x in parts[0].split("-")]
+        elf2 = [int(x) for x in parts[1].split("-")]
+
+        if fn(elf1, elf2):
+            count += 1
+
+    return count
+
+
+def part1_condition(elf1: List[int], elf2: List[int]) -> bool:
+    assert len(elf1) == 2
+    assert len(elf2) == 2
+
+    (x1, x2) = elf1
+    (y1, y2) = elf2
+
+    return (x1 <= y1 <= y2 <= x2) or (y1 <= x1 <= x2 <= y2)
+
+
+def part2_condition(elf1: List[int], elf2: List[int]) -> bool:
+    assert len(elf1) == 2
+    assert len(elf2) == 2
+
+    (x1, x2) = elf1
+    (y1, y2) = elf2
+
+    return (
+        x1 <= y1 <= y2 <= x2
+        or y1 <= x1 < +x2 <= y2
+        or x1 <= y1 <= x2
+        or x1 <= y2 <= x2
+        or y1 <= x1 <= y2
+        or y1 <= x2 <= y2
+    )
+
+
+def parse_input(input: str) -> List[str]:
+    with open(input) as fn:
+        raw_data = fn.read().strip()
+        parts = raw_data.split("\n")
+    return parts
+
+
+def main():
+    print(f"AoC2022 Day1, Part1 solution is: {run('input.txt', part1_condition)}")
+    print(f"AoC2022 Day1, Part2 solution is: {run('input.txt', part2_condition)}")
+
+
+if __name__ == "__main__":
+    main()
