b82c6652e2783675f388d01c488ed3bd5186ef45
povarna <vlad.povarna@uipath.com>
2024-05-07 18:50:19 +0300

    refactor

--- python/2022/day09/day09.py
+++ python/2022/day09/day09.py
@@ -1,26 +1,23 @@
 directions = {"R": [1, 0], "U": [0, 1], "D": [0, -1], "L": [-1, 0]}
 
-tx, ty = 0, 0
-hx, hy = 0, 0
-
 def touching(x1, y1, x2, y2):
     return abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1
+
+def move(hx, hy, tx, ty):
+    if not touching(hx, hy, tx, ty):
+        sign_x = 0 if hx == tx else (hx - tx) / abs(hx - tx)
+        sign_y = 0 if hy == ty else (hy - ty) / abs(hy - ty)
+
+        tx += sign_x
+        ty += sign_y
+    return (tx, ty)
 
 
 def part1(input: str) -> int:
     lines = parse_input(input)
-
-    def move(dx, dy):
-        global hx, hy, tx, ty
-        hx += dx
-        hy += dy
-
-        if not touching(hx, hy, tx, ty):
-            sign_x = 0 if hx == tx else (hx - tx) / abs(hx - tx)
-            sign_y = 0 if hy == ty else (hy - ty) / abs(hy - ty)
-
-            tx += sign_x
-            ty += sign_y
+    
+    tx, ty = 0, 0
+    hx, hy = 0, 0
 
     tail_visited = set()
     tail_visited.add((tx, ty))
@@ -31,7 +28,9 @@ def part1(input: str) -> int:
         dx, dy = directions[op]
 
         for _ in range(amount):
-            move(dx, dy)
+            hx += dx
+            hy += dy
+            tx, ty = move(hx, hy, tx, ty)
             tail_visited.add((tx, ty))
 
     return len(tail_visited)
