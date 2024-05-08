110f365891af7497bc142fcb431bba436954fc4c
povarna <vlad.povarna@uipath.com>
2024-05-08 15:37:11 +0300

    AoC2022 Day10 solution

--- python/2022/day10/day10.py
+++ python/2022/day10/day10.py
@@ -33,8 +33,44 @@ def update_ans(op: int, X: int) -> int:
     return 0 if op not in interesting_points else op * X
 
 
-def part2(input: str) -> int:
-    return -1
+def part2(input: str):
+    lines = parse_input(input)
+        
+    X = 1
+    op = 0
+    
+    crt = [1] * 241
+    
+    
+    for line in lines:
+        parts = line.split(" ")
+        if parts[0] == "noop":
+          op += 1
+          crt[op] = X
+
+        elif parts[0] == "addx":
+            value = int(parts[1])
+            
+            op += 1
+            crt[op] = X
+            X += value
+
+            op += 1
+            crt[op] = X
+    
+    ans = [[None] * 40 for _ in range(6)]
+
+    for row in range(6):
+        for col in range(40):
+            counter = row * 40 + col + 1
+            if abs(crt[counter - 1] - (col)) <= 1:
+                ans[row][col] = "##"
+            else:
+                ans[row][col] = "  "
+
+
+    for row in ans:
+        print("".join(row))
 
 
 def parse_input(input: str) -> list[str]:
@@ -45,7 +81,8 @@ def parse_input(input: str) -> list[str]:
 def main():
     puzzle_input_path = "2022/day10/input.txt"
     print(f"AoC2022 Day10, Part1 solution is: {part1(input=puzzle_input_path)}")
-    print(f"AoC2022 Day10, Part2 solution is: {part2(input=puzzle_input_path)}")
+    print("AoC2022 Day10, Part2 solution is:")
+    part2(input=puzzle_input_path)
 
 
 if __name__ == "__main__":
