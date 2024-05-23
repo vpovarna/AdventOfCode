f1e2654e46a67239a5a262adb866beae0f2a6db9
povarna <vlad.povarna@uipath.com>
2024-05-23 21:33:09 +0300

    wip

---
+++ python/2022/day20/day20.py
@@ -0,0 +1,13 @@
+
+def part1(input_str: str) -> int:
+    return -1
+
+
+def main():
+    puzzle_input_path = "2022/day20/input.txt"
+    print(f"AoC2022 Day20, Part1 solution is: {part1(input_str=puzzle_input_path)}")
+    print(f"AoC2022 Day20, Part2 solution is: {part2(input_str=puzzle_input_path)}")
+
+
+if __name__ == "__main__":
+    main()
