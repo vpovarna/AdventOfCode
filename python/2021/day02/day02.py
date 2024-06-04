865c328c472862ec0b958fdf2df129af7ecb622d
Vlad Povarna <povarna@mbp-vlad.eur.adobe.com>
2024-06-04 15:57:20 +0300

    AoC2022 Day2, part1 solutions

---
+++ python/2021/day02/day02.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+def part1(puzzle_input_path: str) -> int:
+    lines = read_input(input_file=puzzle_input_path)
+    horizontal, depth = 0, 0
+
+    for line in lines:
+        direction, value = line.split(" ")
+        value = int(value)
+
+        match direction:
+            case "forward":
+                horizontal += value
+            case "up":
+                depth -= value
+            case "down":
+                depth += value
+            case _:
+                raise Exception("Unsupported direction")
+
+    return horizontal * depth
+
+
+def part2(puzzle_input_path: str) -> int:
+    return -1
+
+
+def read_input(input_file: str) -> List[str]:
+    with open(input_file) as fn:
+        return fn.read().strip().splitlines()
+
+
+def main():
+    puzzle_input_path = "2021/day02/input.txt"
+    print(f"AoC2021 Day2, Part1 solution is: {part1(puzzle_input_path)}")
+    print(f"AoC2021 Day2, Part2 solution is: {part2(puzzle_input_path)}")
+
+
+if __name__ == '__main__':
+    main()
