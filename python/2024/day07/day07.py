091e8eb6a984e7e359d7c32449ee506db8a2fbc7
povarna <povarna@adobe.com>
2024-12-07 17:21:27 +0200

    @trivial simplify solution

--- python/2024/day07/day07.py
+++ python/2024/day07/day07.py
@@ -1,8 +1,6 @@
 from dataclasses import dataclass
 from typing import List
 
-import tqdm
-
 
 @dataclass
 class Equation:
@@ -21,99 +19,28 @@ with open("input.txt") as f:
         equations.append(Equation(target_value, numbers))
 
 
-def generate_operations(arr: List[int], current: str = "") -> List[str]:
-    if len(arr) == 1:
-        return [current + str(arr[0])]
-
-    results = []
-
-    first = arr[0]
-    rest = arr[1:]
-
-    for expression in generate_operations(rest, ""):
-        results.append(f"{current}{first} + {expression}")
-        results.append(f"{current}{first} * {expression}")
-
-    return results
+def is_valid(target: int, ns: list[int], part2: bool) -> bool:
+    if len(ns) == 1:
+        return ns[0] == target
+    if is_valid(target, [ns[0] + ns[1]] + ns[2:], part2):
+        return True
+    if is_valid(target, [ns[0] * ns[1]] + ns[2:], part2):
+        return True
+    if part2 and is_valid(target, [int(str(ns[0]) + str(ns[1]))] + ns[2:], part2):
+        return True
+    return False
 
 
-def generate_and_calculate(arr, current_result=None) -> set[int]:
-    if len(arr) == 1:
-        if current_result is None:
-            return {arr[0]}
-        else:
-            return {current_result + arr[0], current_result * arr[0]}
-
-    unique_results = set()
-
-    if current_result is None:
-        current_result = arr[0]
-        rest = arr[1:]
-        return generate_and_calculate(rest, current_result)
-
-    rest = arr[1:]
-    unique_results.update(generate_and_calculate(rest, current_result + arr[0]))
-    unique_results.update(generate_and_calculate(rest, current_result * arr[0]))
-
-    return unique_results
-
-
-def generate_and_calculate_with_concat(arr, current_result=None) -> set[int]:
-    if len(arr) == 1:
-        if current_result is None:
-            return {arr[0]}
-        else:
-            return {
-                current_result + arr[0],  # Addition
-                current_result * arr[0],  # Multiplication
-                int(str(current_result) + str(arr[0])),  # Concatenation
-            }
-
-    unique_results = set()
-
-    if current_result is None:
-        current_result = arr[0]
-        rest = arr[1:]
-        return generate_and_calculate_with_concat(rest, current_result)
-
-    rest = arr[1:]
-    unique_results.update(
-        generate_and_calculate_with_concat(rest, current_result + arr[0])
-    )  # Addition
-    unique_results.update(
-        generate_and_calculate_with_concat(rest, current_result * arr[0])
-    )  # Multiplication
-    unique_results.update(
-        generate_and_calculate_with_concat(rest, int(str(current_result) + str(arr[0])))
-    )  # Concatenation
-
-    return unique_results
-
-
-def part1() -> int:
+def run(is_part2: bool = False) -> int:
     total = 0
 
     for equation in equations:
-        for result in generate_and_calculate(equation.numbers):
-            if result == equation.target_value:
-                total += equation.target_value if result == equation.target_value else 0
-                break
-
-    return total
-
-
-def part2() -> int:
-    total = 0
-
-    for equation in equations:
-        for result in generate_and_calculate_with_concat(equation.numbers):
-            if result == equation.target_value:
-                total += equation.target_value if result == equation.target_value else 0
-                break
+        if is_valid(equation.target_value, equation.numbers, is_part2):
+            total += equation.target_value
 
     return total
 
 
 if __name__ == '__main__':
-    print(f"Part 1: {part1()}")
-    print(f"Part 2: {part2()}")
+    print(f"Part 1: {run(is_part2=False)}")
+    print(f"Part 2: {run(is_part2=True)}")
