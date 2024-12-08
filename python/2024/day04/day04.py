0bdd84d740eadec4b81e57c11a263ff27952c008
povarna <povarna@adobe.com>
2024-12-08 14:34:09 +0200

    AoC2024 Day04 solutions

--- python/2024/day04/day04.py
+++ python/2024/day04/day04.py
@@ -1,8 +1,8 @@
 with open("input.txt") as f:
-    lines = f.read().strip().split("\n")
+    grid = f.read().strip().split("\n")
 
-M = len(lines)
-N = len(lines[0])
+M = len(grid)
+N = len(grid[0])
 
 directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]
 
@@ -12,12 +12,22 @@ def has_xmax(i: int, j: int, direction: tuple[int, int]) -> bool:
         x = i + idx * direction[0]
         y = j + idx * direction[1]
 
-        if 0 <= x < M and 0 <= y < N:
-            return False
-        if lines[x][y] != c:
+        if not (0 <= x < M and 0 <= y < N) or grid[x][y] != c:
             return False
 
     return True
+
+
+def has_xmax_on_diagonals(i: int, j: int) -> bool:
+    if not (1 <= i < M - 1 and 1 <= j < N - 1):
+        return False
+    if grid[i][j] != "A":
+        return False
+
+    diag_1 = f"{grid[i - 1][j - 1]}{grid[i + 1][j + 1]}"
+    diag_2 = f"{grid[i - 1][j + 1]}{grid[i + 1][j - 1]}"
+
+    return (diag_1 == "MS" or diag_1 == "SM") and (diag_2 == "MS" or diag_2 == "SM")
 
 
 def part1() -> int:
@@ -32,7 +42,13 @@ def part1() -> int:
 
 
 def part2() -> int:
-    1
+    ans = 0
+
+    for i in range(M):
+        for j in range(N):
+            if has_xmax_on_diagonals(i, j):
+                ans += 1
+    return ans
 
 
 if __name__ == '__main__':
