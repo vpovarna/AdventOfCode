e22484658d8e2d4e2d7d5f5f131963db9c0a4d85
povarna <povarna@adobe.com>
2024-12-09 10:17:51 +0200

    AoC2024 Day09 part2 solution

--- python/2024/day09/day09.py
+++ python/2024/day09/day09.py
@@ -3,29 +3,72 @@ with open("input.txt") as f:
 
 
 def part1() -> int:
-    arr = []
+    files = []
     idx = 0
 
     for i, c in enumerate(input):
 
         if i % 2 != 0:
-            arr += [-1] * int(c)
+            files += [-1] * int(c)
         else:
-            arr += [idx] * int(c)
+            files += [idx] * int(c)
             idx += 1
 
-    blanks = [i for i, c in enumerate(arr) if c == -1]
+    blanks = [i for i, c in enumerate(files) if c == -1]
 
     for i in blanks:
         # clean up the -1 from the end of the array
-        while arr[-1] == -1:
-            arr.pop()
-        if len(arr) <= i:
+        while files[-1] == -1:
+            files.pop()
+        if len(files) <= i:
             break
-        arr[i] = arr.pop()
+        files[i] = files.pop()
 
-    return sum([i * x for i, x in enumerate(arr)])
+    # calculate checksum
+    return sum([i * x for i, x in enumerate(files)])
+
+
+def part2() -> int:
+    # dictionary containing a pair of the number of positions and the index of the file
+    files = {}
+    blanks = []
+
+    idx = 0
+    pos = 0
+
+    for i, c in enumerate(input):
+        x = int(c)
+        if i % 2 == 0:
+            files[idx] = (pos, x)
+            idx += 1
+        else:
+            blanks.append((pos, x))
+        pos += x
+
+    while idx > 0:
+        idx -= 1
+        pos, size = files[idx]
+        for i, (start, length) in enumerate(blanks):
+            if start > pos:
+                blanks = blanks[:i]
+                break
+            if size <= length:
+                files[idx] = (start, size)
+                if size == length:
+                    blanks.pop(i)
+                else:
+                    blanks[i] = (start + size, length - size)
+                break
+
+    # calculate checksum
+    total = 0
+    for i, (pos, size) in files.items():
+        for x in range(pos, pos + size):
+            total += x * i
+
+    return total
 
 
 if __name__ == '__main__':
     print(f"Part1: {part1()}")
+    print(f"Part2: {part2()}")
